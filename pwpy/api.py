ffcfab4d9bc445c54f43d6f4292712f5becf2031
Kyle Tyo <akikai297@gmail.com>
2022-01-27 12:07:48 -0500

    changed type hinting on fetch_query

--- pwpy/api.py
+++ pwpy/api.py
@@ -27,7 +27,7 @@ import aiohttp
 import typing
 
 
-async def fetch_query(key: str, query: str, keys: typing.Iterable = None) -> dict:
+async def fetch_query(key: str, query: str, keys: typing.Iterable = None) -> typing.Any[dict, int]:
     """
     Fetches a given query from the gql api using a provided api key.
 
