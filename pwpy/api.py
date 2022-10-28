2f27fcc79a92002175f4f78e9ad50e15ba374b04
Kyle Tyo <akikai297@gmail.com>
2022-10-28 10:33:52 -0400

    Update api.py

--- pwpy/api.py
+++ pwpy/api.py
@@ -15,201 +15,87 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from pwpy import exceptions, urls, utils
+from pwpy import urls, utils
 
+import asyncio
 import aiohttp
 import typing
 
 
-TOKEN = None
+__all__ = [
+    "set_global_key",
+    "fetch_query",
+    "BulkQuery"
+]
 
 
-def set_token(token: str) -> None:
+API_KEY = ""
+
+
+def set_global_key(api_key: str) -> None:
     """
-    Sets a global token to be used when one is not provided to a method.
+    Set a package level api key to be used for all queries where no key is provided.
     """
-    global TOKEN
-    TOKEN = token
+    global API_KEY
+    API_KEY = api_key
 
 
-def _parse_errors(data: dict) -> None:
-    """
-    Parse return data for errors and raise accordingly.
-    """
-    def interpret_errors(errors):
-        message = errors[0]["message"]
-
-        if "invalid api_key" in message:
-            raise exceptions.InvalidToken(message)
-
-        elif "Syntax Error" in message:
-            raise exceptions.InvalidQuery(message)
-
-        else:
-            raise exceptions.UnexpectedResponse(message)
-
-    if isinstance(data, dict):
-        if "errors" in data.keys():
-            interpret_errors(data["errors"])
-
-        elif "data" in data.keys():
-            return
-
-    elif isinstance(data, list):
-        interpret_errors(data[0]["errors"])
-
-    raise exceptions.UnexpectedResponse(str(data))
-
-
-def _parse_variables(variables: dict) -> list:
-    """
-    Parse an iterable of provided variables into string format.
-    """
-    parsed = []
-
-    for section, element in variables.items():
-        if isinstance(element, str):
-            parsed.append(f"{section} {{{element}}}")
-
-        elif isinstance(element, typing.Iterable):
-            local = []
-
-            for item in element:
-                if isinstance(item, dict):
-                    local.append(" ".join(_parse_variables(item)))
-
-                elif isinstance(item, str):
-                    local.append(item)
-
-            parsed.append(f"{section} {{{' '.join(local)}}}")
-
-    return parsed
-
-
-def _parse_query(query: dict) -> str:
-    """
-    Parse a provided dictionary into a formatted gql string.
-    """
-    parsed_queries = []
-
-    for name, entry in query.items():
-        parsed_args = " ".join(f"{key}:{value}" for key, value in entry["args"].items())
-        parsed_variables = " ".join(_parse_variables(entry["variables"]))
-        parsed_queries.append(f"{name}({parsed_args}) {{{parsed_variables}}}")
-
-    return " ".join(parsed_queries)
-
-
-async def get(
-    query: dict, *,
-    token: str = TOKEN,
-    keys: typing.Iterable[str] = None
-) -> typing.Any:
+async def fetch_query(query: dict or str, *, api_key: str = API_KEY) -> typing.Any:
     """
     Fetches a given query from the gql api using a provided api key.
 
-    :param token: A valid Politics and War API key.
-    :param query: A query dictionary object.
-    :param keys: A list of keys to parse the response with.
+    :param query: A query formatted as a dict.
+    :param api_key: A valid Politics and War API key.
     :return: A dictionary response from the server.
     """
-    query = _parse_query(query)
+    if isinstance(query, dict):
+        query = utils.parse_query(query)
 
     async with aiohttp.ClientSession() as session:
-        async with session.post(urls.API + token, json={"query": f"{{{query}}}"}) as response:
-            if not response.ok:
-                raise exceptions.CloudflareInterrupt("cloudflare error encountered while trying to post query!")
+        async with session.post(urls.API + api_key, json={"query": f"{{{query}}}"}) as response:
+            response = await response.json()
 
-            data = await response.json()
+    utils.parse_errors(response)
 
-    _parse_errors(data)
-
-    data = data["data"]
-    for key in keys:
-        data = data[key]
-
-    return data
+    return response["data"]
 
 
-async def within_war_range(
-    score: int, *,
-    alliance: int = None,
-    powered: bool = True,
-    omit_alliance: int = None,
-    token: str = TOKEN
-) -> list:
-    """
-    Lookup all targets for a given score meeting optional criteria.
+class BulkQuery:
 
-    :param score: Score to be calculated with.
-    :param alliance: Target alliance to narrow the search. Defaults to 0.
-    :param powered: Whether to discriminate against unpowered cities. Defaults to True.
-    :param omit_alliance: An alliance to be omitted from search results.
-    :param token: A valid Politics and War API key.
-    :return: A list of nations that fall within the provided search criteria.
-    """
-    min_score, max_score = utils.score_range(score)
+    __slots__: typing.List = [
+        "_page_groups",
+        "_queries",
+    ]
 
-    query = {
-        "nations": {
-            "args": {
-                "first": 100,
-                "min_scored": min_score,
-                "max_score": max_score,
-                "vacation_mode": False
-            },
-            "variables": {
-                "data": {
-                    "id",
-                    "nation_name",
-                    "leader_name",
-                    "color",
-                    "alliance_id",
-                    "alliance_position",
-                    {"alliance": ("name", "score")},
-                    "war_policy",
-                    "flag",
-                    "num_cities",
-                    "score",
-                    "espionage_available",
-                    "last_active",
-                    "soldiers",
-                    "tanks",
-                    "aircraft",
-                    "ships",
-                    "missiles",
-                    "nukes",
-                    {"cities": "powered"},
-                    {"offensive_wars": ("id", "winner", "turns_left")},
-                    {"defensive_wars": ("id", "winner", "turns_left")}
-                }
-            }
-        }
-    }
+    def __init__(self):
+        self._queries: list = []
 
-    if alliance:
-        query["nations"]["args"]["alliance_id"] = alliance
+    @staticmethod
+    def _chunk_requests(iterable: typing.Sized, length):
+        for count in range(0, len(iterable), length):
+            chunk = {}
 
-    targets = await get(query, token=token, keys=("nations", "data"))
+            for entry in iterable[count:count + length]:
+                chunk.update(entry)
 
-    for nation in targets[::]:
-        ongoing = utils.sort_ongoing_wars(nation["defensive_wars"])
-        if nation["alliance_id"] == omit_alliance:
-            targets.remove(nation)
+            yield chunk
 
-        elif nation["color"] == "beige":
-            targets.remove(nation)
+    def insert(self, query: dict) -> None:
+        self._queries.append(query)
 
-        elif len(ongoing) == 3:
-            targets.remove(nation)
+    async def fetch_query(self, *, api_key: str = API_KEY, chunk_size: int = 10) -> dict:
+        results = {}
 
-        elif powered:
-            for city in nation["cities"]:
-                if city["powered"]:
-                    continue
+        chunk_size = chunk_size if chunk_size > 0 else 1
+        chunks = self._chunk_requests(self._queries, chunk_size)
+        tasks = set()
 
-                targets.remove(nation)
-                break
+        for chunk in chunks:
+            tasks.add(asyncio.create_task(fetch_query(chunk, api_key=api_key)))
 
-    return targets
+        response = await asyncio.gather(*tasks)
 
+        for chunk in response:
+            results.update(chunk)
+
+        return results
