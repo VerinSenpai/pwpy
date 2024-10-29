0dc2aba64139852cf64cec28be04ea575b2f9a57
Kyle Tyo <akikai297@gmail.com>
2024-10-28 20:06:04 -0400

    add references for model fields

--- pwpy/models.py
+++ pwpy/models.py
@@ -32,6 +32,42 @@ class _Base:
 
 
 @attr.s(auto_attribs=True)
+class SocialPolicy(Enum):
+    ANARCHIST = "ANARCHIST"
+    LIBERTARIAN = "LIBERTARIAN"
+    LIBERAL = "LIBERAL"
+    MODERATE = "MODERATE"
+    CONSERVATIVE = "CONSERVATIVE"
+    AUTHORITARIAN = "AUTHORITARIAN"
+    FASCIST = "FASCIST"
+
+
+@attr.s(auto_attribs=True)
+class WarPolicy(Enum):
+    ATTRITION = "ATTRITION"
+    TURTLE = "TURTLE"
+    BLITZKRIEG = "BLITZKRIEG"
+    FORTRESS = "FORTRESS"
+    MONEYBAGS = "MONEYBAGS"
+    PIRATE = "PIRATE"
+    TACTICIAN = "TACTICIAN"
+    GUARDIAN = "GUARDIAN"
+    COVERT = "COVERT"
+    ARCANE = "ARCANE"
+
+
+@attr.s(auto_attribs=True)
+class EconomicPolicy(Enum):
+    EXTREME_LEFT = "EXTREME_LEFT"
+    FAR_LEFT = "FAR_LEFT"
+    LEFT = "LEFT"
+    MODERATE = "MODERATE"
+    RIGHT = "RIGHT"
+    FAR_RIGHT = "FAR_RIGHT"
+    EXTREME_RIGHT = "EXTREME_RIGHT"
+
+
+@attr.s(auto_attribs=True)
 class DomesticPolicy(Enum):
     MANIFEST_DESTINY = "MANIFEST_DESTINY"
     OPEN_MARKETS = "OPEN_MARKETS"
@@ -39,6 +75,145 @@ class DomesticPolicy(Enum):
     IMPERIALISM = "IMPERIALISM"
     URBANIZATION = "URBANIZATION"
     RAPID_EXPANSION = "RAPID_EXPANSION"
+
+
+@attr.s(auto_attribs=True)
+class BulletinType(Enum):
+    nation: int = 1
+    alliance: int = 2
+
+
+@attr.s(auto_attribs=True)
+class GovernmentType(Enum):
+    ABSOLUTE_MONARCHY = "ABSOLUTE_MONARCHY"
+    ANARCHY = "ANARCHY"
+    ARISTOCRACY = "ARISTOCRACY"
+    BANANA_REPUBLIC = "BANANA_REPUBLIC"
+    COMMUNIST_DEMOCRACY = "COMMUNIST_DEMOCRACY"
+    COMMUNIST_DICTATORSHIP = "COMMUNIST_DICTATORSHIP"
+    COMMUNIST_MONARCHY = "COMMUNIST_MONARCHY"
+    COMMUNIST_REPUBLIC = "COMMUNIST_REPUBLIC"
+    COMMUNIST_THEOCRACY = "COMMUNIST_THEOCRACY"
+    CONSTITUTIONAL_MONARCHY = "CONSTITUTIONAL_MONARCHY"
+    CONSTITUTIONAL_REPUBLIC = "CONSTITUTIONAL_REPUBLIC"
+    DEMARCHY = "DEMARCHY"
+    DEMOCRACY = "DEMOCRACY"
+    DEMOCRATIC_REPUBLIC = "DEMOCRATIC_REPUBLIC"
+    DICTATORSHIP = "DICTATORSHIP"
+    FEDERAL_REPUBLIC = "FEDERAL_REPUBLIC"
+    MONARCHY = "MONARCHY"
+    NOOCRACY = "NOOCRACY"
+    OLIGARCHY = "OLIGARCHY"
+    PARLIAMENTARY_DEMOCRACY = "PARLIAMENTARY_DEMOCRACY"
+    PARLIAMENTARY_REPUBLIC = "PARLIAMENTARY_REPUBLIC"
+    PEOPLES_REPUBLIC = "PEOPLES_REPUBLIC"
+    REPUBLIC = "REPUBLIC"
+    SOCIAL_DEMOCRACY = "SOCIAL_DEMOCRACY"
+    SOCIALIST_DICTATORSHIP = "SOCIALIST_DICTATORSHIP"
+    SOCIALIST_REPUBLIC = "SOCIALIST_REPUBLIC"
+    SOCIALIST_THEOCRACY = "SOCIALIST_THEOCRACY"
+    STRATOCRACY = "STRATOCRACY"
+    TECHNOCRACY = "TECHNOCRACY"
+    THEOCRACY = "THEOCRACY"
+    THEOCRATIC_DEMOCRACY = "THEOCRATIC_DEMOCRACY"
+    THEOCRATIC_DICTATORSHIP = "THEOCRATIC_DICTATORSHIP"
+    THEOCRATIC_REPUBLIC = "THEOCRATIC_REPUBLIC"
+    MINDHIVE = "MINDHIVE"
+
+
+@attr.s(auto_attribs=True)
+class BulletinReply(_Base):
+    id: int = None
+    date: Any = None
+    nation_id: int = None
+    nation: "Nation" = attr.ib(factory=lambda: Nation)
+    bulletin_id: int = None
+    message: str = None
+    edit_date: int = None
+    nation_name: str = None
+    leader_name: str = None
+    like_count: int = None
+
+
+@attr.s(auto_attribs=True)
+class Bulletin(_Base):
+    id: int = None
+    nation_id: int = None
+    nation: "Nation" = attr.ib(factory=lambda: Nation)
+    alliance_id: int = None
+    alliance: "Alliance" = attr.ib(factory=lambda: Alliance)
+    type: BulletinType = None
+    headline: str = None
+    excerpt: str = None
+    image: str = None
+    body: str = None
+    author: str = None
+    pinned: bool = None
+    like_count: int = None
+    replies_enabled: bool = None
+    locked: bool = None
+    date: Any = None  # <---- Type and conversion
+    edit_date: Any = None  # <---- Type and conversion
+    archived: bool = None
+    replies: List[BulletinReply] = None
+
+
+
+@attr.s(auto_attribs=True)
+class War(_Base):
+    id: int = None
+    date: Any = None  # <---- Type and conversion
+    end_date: Any = None  # <---- Type and conversion
+    reason: str = None
+    war_type: Any = None  # <---- Type and conversion
+    ground_control: int = None
+    air_superiority: int = None
+    naval_blockade: int = None
+    winner_id: int = None
+    attacks: Any = None  # <---- Type and conversion
+    turns_left: int = None
+    att_id: int = None
+    att_alliance_id: int = None
+    att_alliance_position: Any = None  # <---- Type and conversion
+    attacker: "Nation" = attr.ib(factory=lambda: Nation)
+    def_id: int = None
+    def_alliance_id: int = None
+    def_alliance_position: Any = None
+    defender: "Nation" = attr.ib(factory=lambda: Nation)
+    att_points: int = None
+    def_points: int = None
+    att_peace: bool = None
+    def_peace: bool = None
+    att_resistance: int = None
+    def_resistance: int = None
+    att_fortify: bool = None
+    def_fortify: bool = None
+    att_gas_used: float = None
+    def_gas_used: float = None
+    att_mun_used: float = None
+    def_mun_used: float = None
+    att_alum_used: float = None
+    def_alum_used: float = None
+    att_steel_used: float = None
+    def_steel_used: float = None
+    att_infra_destroyed: float = None
+    def_infra_destroyed: float = None
+    att_money_looted: float = None
+    def_money_looted: float = None
+    def_soldiers_lost: int = None
+    att_soldiers_lost: int = None
+    def_tanks_lost: int = None
+    att_tanks_lost: int = None
+    def_aircraft_lost: int = None
+    att_aircraft_lost: int = None
+    def_ships_lost: int = None
+    att_ships_lost: int = None
+    att_missiles_used: int = None
+    def_missiles_used: int = None
+    att_nukes_used: int = None
+    def_nukes_used: int = None
+    att_infra_destroyed_value: float = None
+    def_infra_destroyed_value: float = None
 
 
 @attr.s(auto_attribs=True)
@@ -108,8 +283,8 @@ class Nation(_Base):
     bauxite_works: bool = None
     beige_turns: int = None
     bounties: Any = None  # <---- Type and conversion
-    bulletin_replies: Any = None  # <---- Type and conversion
-    bulletins: Any = None  # <---- Type and conversion
+    bulletin_replies: List[BulletinReply] = None
+    bulletins: List[Bulletin] = None
     bureau_of_domestic_affairs: bool = None
     center_for_civil_engineering: bool = None
     central_intelligence_agency: bool = None
@@ -128,7 +303,7 @@ class Nation(_Base):
     discord_id: int = None
     domestic_policy: DomesticPolicy = None
     domestic_policy_turns: int = None
-    economic_policy: Any = None  # <---- Type and conversion
+    economic_policy: EconomicPolicy = None
     emergency_gasoline_reserve: bool = None
     espionage_available: bool = None
     fallout_shelter: bool = None
@@ -136,7 +311,7 @@ class Nation(_Base):
     food: float = None
     gasoline: float = None
     government_support_agency: bool = None
-    government_type: Any = None  # <---- Type and conversion
+    government_type: GovernmentType = None
     green_technologies: bool = None
     gross_domestic_product: float = None
     gross_national_income: float = None
@@ -172,7 +347,7 @@ class Nation(_Base):
     nukes: int = None
     nukes_today: int = None
     num_cities: int = None
-    offensive_war_count: int = None
+    offensive_wars_count: int = None
     oil: float = None
     pirate_economy: bool = None
     population: int = None
@@ -189,7 +364,7 @@ class Nation(_Base):
     ship_kills: int = None
     ships: int = None
     ships_today: int = None
-    social_policy: Any = None  # <---- Type and conversion
+    social_policy: SocialPolicy = None
     soldier_casualties: int = None
     soldier_kills: int = None
     soldiers: int = None
@@ -224,9 +399,9 @@ class Nation(_Base):
     vacation_mode_turns: int = None
     vip: bool = None
     vital_defense_system: bool = None
-    war_policy: Any = None  # <---- Type and conversion
+    war_policy: WarPolicy = None
     war_policy_turns: int = None
-    wars: Any = None  # <---- Type and conversion
+    wars: List[War] = None
     wars_lost: int = None
     wars_won: int = None
 
@@ -285,7 +460,7 @@ class Alliance(_Base):
     awards: Any = None  # <---- Type and conversion
     bankrecs: Any = None  # <---- Type and conversion
     bauxite: float = None
-    bulletins: Any = None  # <---- Type and conversion
+    bulletins: List[Bulletin] = None
     coal: float = None
     color: str = None
     date: Any = None
@@ -311,7 +486,7 @@ class Alliance(_Base):
     taxrecs: Any = None  # <---- Type and conversion
     treaties: Any = None  # <---- Type and conversion
     uranium: float = None
-    wars: Any = None  # <---- Type and conversion
+    wars: List[War] = None
     wiki_link: str = None
 
     @property
